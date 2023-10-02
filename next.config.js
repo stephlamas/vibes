58d5435615e571920b944729da1452220dfd49eb
stephlamas <estefania.lamas@coverwallet.com>
2023-10-02 22:56:09 +0200

    config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
    experimental: {
     appDir: true
    },
+   env: {
+      CLIENT_ID: process.env.CLIENT_ID,
+      CLIENT_SECRET: process.env.CLIENT_SECRET,
+      REDIRECT_URI: process.env.REDIRECT_URI,
+   }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
