a00fd22e6763287d5bf6d593c1be6654de2cc702
stephlamas <estefania.lamas@coverwallet.com>
2023-10-29 23:50:34 +0100

    bottom nav navigation

--- app/layout/components/bottom-nav/bottom-nav.tsx
+++ app/layout/components/bottom-nav/bottom-nav.tsx
@@ -3,22 +3,33 @@ import { bottomNavStyles } from "./styles/bottom-nav.styles";
 import { HomeIcon } from "../icons/sidebar/Home";
 import { EventsIcon } from "../icons/sidebar/Events";
 import { ArtistsIcon } from "../icons/sidebar/Artists";
-
+import Link from "next/link";
 export function BottomNav() {
   return (
-    <BottomNavigation
-      showLabels
-      sx={bottomNavStyles}
-    >
-      <BottomNavigationAction
-        icon={<HomeIcon />}
-      />
-      <BottomNavigationAction
-        icon={<EventsIcon />}
-      />
-      <BottomNavigationAction
-        icon={<ArtistsIcon />}
-      />
+    <BottomNavigation showLabels sx={bottomNavStyles}>
+      <Link href="/home" passHref>
+        <BottomNavigationAction
+          icon={<HomeIcon />}
+          label="Home"
+          value="home"
+        />
+      </Link>
+
+      <Link href="/my-events" passHref>
+        <BottomNavigationAction
+          icon={<EventsIcon />}
+          label="My events"
+          value="my-events"
+        />
+      </Link>
+
+      <Link href="/my-artists" passHref>
+        <BottomNavigationAction
+          icon={<ArtistsIcon />}
+          label="My artists"
+          value="my-artists"
+        />
+      </Link>
     </BottomNavigation>
   );
 }
--- app/layout/components/bottom-nav/styles/bottom-nav.styles.tsx
+++ app/layout/components/bottom-nav/styles/bottom-nav.styles.tsx
@@ -2,4 +2,5 @@ export const bottomNavStyles = {
     width: "100%",
     position: "fixed",
     bottom: 0,
+    justifyContent: "space-around",
 };
