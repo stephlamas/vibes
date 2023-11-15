ea2cc055c11731d16d19ed6ffbc9bc2c0397927c
Estefania Lamas <esteflamas@gmail.com>
2023-11-15 20:53:07 +0100

    remove muibackdrop

--- core/theme/components/MuiBackdrop.tsx
+++
@@ -1,9 +0,0 @@
-export const MuiBackdrop = {
-  styleOverrides: {
-    root: {
-      borderRadius: '12px',
-      backgroundColor: 'rgba(255, 255, 255, 0.10)',
-      padding: '2em',
-    },
-  },
-};
--- core/theme/components/index.tsx
+++ core/theme/components/index.tsx
@@ -1,11 +1,9 @@
 import { MuiTypography } from "./MuiTypography";
-import { MuiBackdrop } from "./MuiBackdrop";
 import { MuiButton } from "./MuiButton";
 import { MuiLink } from "./MuiLink";
 
 export const createComponents = () => ({
     MuiTypography,
-    MuiBackdrop,
     MuiButton,
     MuiLink
 });
