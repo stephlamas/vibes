6b493f70fa0c48e05f3f1e14eef6c285e8daa64e
Estefania Lamas <esteflamas@gmail.com>
2023-11-20 23:14:21 +0100

    event page

--- app/(logged)/events/[eventID]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function EventPage(id: any) {
-  return `Welcome to event with id: {id}`;
-}
---
+++ app/(logged)/events/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function({ params } : any) {
+    return <div>Welcome to the event page for event: {params.id}</div>;
+}
