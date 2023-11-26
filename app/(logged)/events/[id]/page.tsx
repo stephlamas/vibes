0a989e7317ff7297e1b37c719493eac1befce0bb
Estefania Lamas <esteflamas@gmail.com>
2023-11-26 20:05:39 +0100

    event page info

--- app/(logged)/events/[id]/page.tsx
+++ app/(logged)/events/[id]/page.tsx
@@ -24,19 +24,18 @@ export default function EventPage({ params }: any) {
         <Box>
             {eventData && (
                 <Box>
-                    {eventData.images && eventData.images.length > 0 && (
-                        <Box
-                            sx={{
-                                backgroundImage: `url(${eventData.images[5].url})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'top',
-                                height: { xs: '250px', md: '500px' },
-                                width: '100%',
-                                borderRadius: '18px',
-                            }}
-                        />
-                    )}
-
+                   {eventData.images && eventData.images.length > 0 && (
+                    <Box
+                        sx={{
+                            backgroundImage: `url(${eventData.images[5].url})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'top',
+                            height: { xs: '250px', md: '500px' },
+                            width: '100%',
+                            borderRadius: '18px',
+                        }}
+                    />
+                )}
                     <Box>
                         <Typography variant="TITLE_S">{eventData.name}</Typography>
                     </Box>
@@ -44,6 +43,9 @@ export default function EventPage({ params }: any) {
                         <Typography variant="PARAGRAPH_S">{eventData?.dates?.start?.localDate}</Typography>
                         <Typography variant="PARAGRAPH_S">{eventData?.dates?.start?.localTime}</Typography>
                         <Typography variant="PARAGRAPH_S">{eventData.priceRanges?.[0]?.min}</Typography>
+                        <Typography variant="PARAGRAPH_S">{eventData.priceRanges?.[0]?.currency}</Typography>
+                        <Typography variant="PARAGRAPH_S">{eventData._embedded?.venues?.[0]?.city?.name}</Typography>
+                        <Typography variant="PARAGRAPH_S">{eventData._embedded?.venues?.[0].name}</Typography>
                     </Box>
                 </Box>
             )}
