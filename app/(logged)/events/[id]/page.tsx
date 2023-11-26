f14e291f1f4a06000476570d9880789abaa65f9c
Estefania Lamas <esteflamas@gmail.com>
2023-11-26 20:08:18 +0100

    event page format currency

--- app/(logged)/events/[id]/page.tsx
+++ app/(logged)/events/[id]/page.tsx
@@ -4,6 +4,35 @@ import { getEventById } from '@/core/services/events-service';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 
+function formatCurrency(price: number | string, currency: string): string {
+    const currencySymbols: Record<string, string> = {
+        USD: "$",
+        EUR: "€",
+        GBP: "£",
+        CAD: "C$",
+        AUD: "A$",
+        MXN: "MX$",
+    };
+
+    if (isNaN(Number(price))) {
+        return price.toString();
+    }
+
+    const currencySymbol = currencySymbols[currency] || "";
+
+    const formattedPrice = new Intl.NumberFormat(undefined, {
+        style: "currency",
+        currency: currency,
+        currencyDisplay: "symbol",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+    }).format(Number(price));
+
+    const numericPart = formattedPrice.replace(/[^\d.,]/g, "");
+
+    return `${currencySymbol}${numericPart}`;
+}
+
 export default function EventPage({ params }: any) {
     const [eventData, setEventData] = useState<any>({});
 
@@ -24,26 +53,27 @@ export default function EventPage({ params }: any) {
         <Box>
             {eventData && (
                 <Box>
-                   {eventData.images && eventData.images.length > 0 && (
-                    <Box
-                        sx={{
-                            backgroundImage: `url(${eventData.images[5].url})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'top',
-                            height: { xs: '250px', md: '500px' },
-                            width: '100%',
-                            borderRadius: '18px',
-                        }}
-                    />
-                )}
+                    {eventData.images && eventData.images.length > 0 && (
+                        <Box
+                            sx={{
+                                backgroundImage: `url(${eventData.images[5].url})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'top',
+                                height: { xs: '250px', md: '500px' },
+                                width: '100%',
+                                borderRadius: '18px',
+                            }}
+                        />
+                    )}
                     <Box>
                         <Typography variant="TITLE_S">{eventData.name}</Typography>
                     </Box>
                     <Box>
                         <Typography variant="PARAGRAPH_S">{eventData?.dates?.start?.localDate}</Typography>
                         <Typography variant="PARAGRAPH_S">{eventData?.dates?.start?.localTime}</Typography>
-                        <Typography variant="PARAGRAPH_S">{eventData.priceRanges?.[0]?.min}</Typography>
-                        <Typography variant="PARAGRAPH_S">{eventData.priceRanges?.[0]?.currency}</Typography>
+                        <Typography variant="PARAGRAPH_S">
+                            {formatCurrency(eventData.priceRanges?.[0]?.min, eventData.priceRanges?.[0]?.currency)}
+                        </Typography>
                         <Typography variant="PARAGRAPH_S">{eventData._embedded?.venues?.[0]?.city?.name}</Typography>
                         <Typography variant="PARAGRAPH_S">{eventData._embedded?.venues?.[0].name}</Typography>
                     </Box>
