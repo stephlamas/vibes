59b7e1865d878406e65c280ed6882e740c6bf06b
Estefania Lamas <esteflamas@gmail.com>
2024-03-18 22:47:35 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
-    appDir: true
-   },
    env: {
       CLIENT_ID: process.env.CLIENT_ID,
       CLIENT_SECRET: process.env.CLIENT_SECRET,
