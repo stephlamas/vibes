6ab0c70486bb67cf791fc382e235510ca9bd202c
Estefania Lamas <esteflamas@gmail.com>
2024-06-25 21:42:54 +0200

    event discovery refactor

--- app/layout/components/event-discovery/event-discovery.tsx
+++ app/layout/components/event-discovery/event-discovery.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback, useMemo } from 'react';
 import { Box, Typography, Button, Skeleton, Container } from '@mui/material';
 import { subtitleTypography } from './styles/event-discoverty.styles';
 import SpotifyClient from '../../../../core/clients/spotify-client';
@@ -26,6 +26,8 @@ type Artist = {
   images: any[];
 };
 
+const PAGE_SIZE = 20;
+
 export function EventDiscovery() {
   const [topArtists, setTopArtists] = useState<Artist[]>([]);
   const [events, setEvents] = useState<Event[]>([]);
@@ -52,9 +54,7 @@ export function EventDiscovery() {
   }, []);
 
   useEffect(() => {
-    if (topArtists.length === 0) {
-      return;
-    }
+    if (topArtists.length === 0) return;
 
     const getUserCountryCode = async () => {
       const spotifyClient = new SpotifyClient();
@@ -68,7 +68,7 @@ export function EventDiscovery() {
             (position) => {
               resolve({
                 latitude: position.coords.latitude,
-                longitude: position.coords.longitude
+                longitude: position.coords.longitude,
               });
             },
             (error) => {
@@ -83,27 +83,27 @@ export function EventDiscovery() {
     };
 
     Promise.all([getUserCountryCode(), getUserLocation()])
-      .then(([userCountryCode, userLocation]: [string | null, { latitude: number; longitude: number }]) => {
-        // console.log('User location:', userLocation);
-        // console.log('User country code:', userCountryCode);
-
+      .then(([userCountryCode, userLocation]) => {
         const eventsPromises = topArtists.map((artist) =>
           fetch(`/api/events?artistName=${encodeURIComponent(artist.name)}`)
-            .then(response => response.json())
-            .catch(err => console.error(err))
+            .then((response) => response.json())
+            .catch((err) => console.error(err))
         );
 
         Promise.all(eventsPromises)
-          .then(eventsArrays => {
-            const allEvents = eventsArrays.flatMap(e => e?._embedded?.events ?? []);
-            const userEvents = allEvents.filter(event => event._embedded?.venues?.[0]?.country?.countryCode === userCountryCode);
-            const otherEvents = allEvents.filter(event => event._embedded?.venues?.[0]?.country?.countryCode !== userCountryCode);
+          .then((eventsArrays) => {
+            const allEvents = eventsArrays.flatMap((e) => e?._embedded?.events ?? []);
+            const userEvents = allEvents.filter(
+              (event) => event._embedded?.venues?.[0]?.country?.countryCode === userCountryCode
+            );
+            const otherEvents = allEvents.filter(
+              (event) => event._embedded?.venues?.[0]?.country?.countryCode !== userCountryCode
+            );
             const startDate = (evt: any) => new Date(evt.dates.start.localDate) as Date;
-            const sortEvents = (e1: any, e2: any) => (startDate(e1) > startDate(e2) ? 1 : -1) as number
+            const sortEvents = (e1: any, e2: any) => (startDate(e1) > startDate(e2) ? 1 : -1) as number;
 
             const eventsWithinRadius = allEvents.filter((event) => {
               const eventLocation = event._embedded?.venues?.[0]?.location;
-              // console.log('Event location:', eventLocation)
               if (!eventLocation) {
                 return false;
               }
@@ -122,26 +122,25 @@ export function EventDiscovery() {
           })
           .catch(console.error);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error:', error);
       });
-
   }, [topArtists]);
 
+  const skeleton = useMemo(
+    () =>
+      Array.from({ length: PAGE_SIZE }).map((_, index) => (
+        <Box key={index} sx={{ mt: 2, width: '100%' }}>
+          <Skeleton variant="rectangular" height={200} sx={{ borderRadius: '20px' }} />
+        </Box>
+      )),
+    []
+  );
 
-
-  const PAGE_SIZE = 20;
-
-  const skeleton = Array.from({ length: PAGE_SIZE }).map((_, index) => (
-    <Box key={index} sx={{ mt: 2, width: '100%' }}>
-      <Skeleton variant="rectangular" height={200} sx={{ borderRadius: '20px' }} />
-    </Box>
-  ))
-
-  const getCurrentPageEvents = () => {
+  const getCurrentPageEvents = useCallback(() => {
     const startIndex = currentPage * PAGE_SIZE;
     return events.slice(startIndex, startIndex + PAGE_SIZE);
-  };
+  }, [currentPage, events]);
 
   const goToNextPage = () => {
     setCurrentPage((prev) => Math.min(prev + 1, totalPages - 1));
@@ -153,20 +152,22 @@ export function EventDiscovery() {
     scrollToTop();
   };
 
-  const scrollToTop = () => {
+  const scrollToTop = useCallback(() => {
     if (topRef.current) {
       topRef.current.scrollIntoView({ behavior: "auto", block: "start" });
     }
-  };
+  }, []);
 
   return (
-    <Container maxWidth="lg" 
-    ref={topRef} 
-    sx={{ 
-      ml: { xs: 0, md: 5 },
-      p: { xs: 0 },
-      mr: { xs: 0 },
-      }}>
+    <Container
+      maxWidth="lg"
+      ref={topRef}
+      sx={{
+        ml: { xs: 0, md: 5 },
+        p: { xs: 0 },
+        mr: { xs: 0 },
+      }}
+    >
       <Box sx={{ p: 2, ml: 1, mt: 10 }}>
         <Typography variant="TITLE_S" component="h1">
           Upcoming events for you
@@ -175,50 +176,52 @@ export function EventDiscovery() {
           Based on your favorite artists
         </Typography>
       </Box>
-      {
-        isLoading ? (
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, marginTop: 2 }} >
-            {skeleton}
-          </Box >
-        ) : events && events.length > 0 && (
+      {isLoading ? (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, marginTop: 2 }}>
+          {skeleton}
+        </Box>
+      ) : (
+        events &&
+        events.length > 0 && (
           <>
-              <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: 3, mb: 8 }}>
-                <Box sx={{ width: '100%', maxWidth: '600px', display: 'flex', flexWrap: 'wrap', gap: 2 }}>
-                  {getCurrentPageEvents().map((event, index) => (
-                    <EventCard
-                      id={event.id}
-                      key={index}
-                      name={event.name}
-                      time={event.dates?.start?.localTime}
-                      date={event.dates?.start?.localDate}
-                      price={event.priceRanges?.[0]?.min}
-                      currency={event.priceRanges?.[0]?.currency}
-                      imageUrl={event.images?.[8]?.url}
-                      city={event._embedded?.venues?.[0]?.city?.name}
-                      venue={event._embedded?.venues?.[0]?.name}
-                      country={event._embedded?.venues?.[0]?.country?.name}
-                    />
-                  ))}
-                </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: 3, mb: 8 }}>
+              <Box sx={{ width: '100%', maxWidth: '600px', display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+                {getCurrentPageEvents().map((event, index) => (
+                  <EventCard
+                    id={event.id}
+                    key={index}
+                    name={event.name}
+                    time={event.dates?.start?.localTime}
+                    date={event.dates?.start?.localDate}
+                    price={event.priceRanges?.[0]?.min}
+                    currency={event.priceRanges?.[0]?.currency}
+                    imageUrl={event.images?.[8]?.url}
+                    city={event._embedded?.venues?.[0]?.city?.name}
+                    venue={event._embedded?.venues?.[0]?.name}
+                    country={event._embedded?.venues?.[0]?.country?.name}
+                  />
+                ))}
               </Box>
+            </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3, mb: 8 }}>
               <Button
                 onClick={goToPreviousPage}
                 disabled={currentPage === 0}
-                style={{ color: currentPage === 0 ? "gray" : "black" }}
+                style={{ color: currentPage === 0 ? 'gray' : 'black' }}
               >
                 Previous
               </Button>
               <Button
                 onClick={goToNextPage}
                 disabled={currentPage >= totalPages - 1}
-                style={{ color: currentPage >= totalPages - 1 ? "gray" : "black", marginLeft: '8px' }}
+                style={{ color: currentPage >= totalPages - 1 ? 'gray' : 'black', marginLeft: '8px' }}
               >
                 Next
               </Button>
             </Box>
           </>
-        )}
+        )
+      )}
     </Container>
-  )
+  );
 }
